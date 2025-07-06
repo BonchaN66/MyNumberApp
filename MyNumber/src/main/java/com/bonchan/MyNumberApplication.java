42019b3b2991b48e7bfa7476474a68487b83cb66
BonchaN(Mac) <naturalcflife@gmail.com>
2025-07-07 02:13:05 +0900

    Add files via upload

---
+++ MyNumber/src/main/java/com/bonchan/MyNumberApplication.java
@@ -0,0 +1,13 @@
+package com.bonchan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyNumberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyNumberApplication.class, args);
+	}
+
+}
